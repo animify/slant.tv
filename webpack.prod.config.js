4f8a7cc45120220ee4b5a62929f814ca886b0756
animify <st.mansson@icloud.com>
2017-10-08 19:15:19 +0300

    Prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -59,9 +59,6 @@ module.exports = {
       test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
       loader: 'file',
     }, {
-      test: /\.json$/,
-      loader: 'file',
-    }, {
       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
       loader: 'url?limit=10000&mimetype=image/svg+xml',
     }]
