17d23a60e72d901da3db9ad605b3d68ec61a1bc2
Stefan Mansson <stefan.aotik@gmail.com>
2018-11-25 16:38:34 +0200

    Public dist for prod

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public', 'dist'),
     publicPath: '/dist/',
   },
 
